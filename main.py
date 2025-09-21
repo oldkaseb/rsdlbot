56f8e9a39cacdfcc2c012c9576dfee1d7c878d90
oldkaseb <hamedsocialworks@gmail.com>
2025-09-21 22:51:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from telegram.ext import (
 from sqlalchemy import (
     create_engine, Column, Integer, String, Boolean, DateTime
 )
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import BigInteger
 
